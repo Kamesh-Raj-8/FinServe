4e42d14a71b09fabaabe8421de47e29d084be399
Kamesh Raj <rajkamesh653@gmail.com>
2026-03-16 10:22:39 +0530

    Initial commit for dev branch

---
+++ README.md
@@ -0,0 +1,9 @@
+# SME-Lend Backend
+
+Generated starter project (Phase-1) with:
+- Java 21 + Spring Boot 3.3.5
+- MySQL + JPA entities from HLD
+- JWT Auth (register/login)
+- Swagger at /swagger
+
+Next: add module controllers/services for each lifecycle stage.
---
+++ src/main/java/com/smelend/smelendbackend/SmeLendBackendApplication.java
@@ -0,0 +1,11 @@
+package com.smelend.smelendbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeLendBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmeLendBackendApplication.class, args);
+    }
+}
